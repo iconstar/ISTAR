fdf44bcaabf678ffea7192e9c21eee64c1ceeb10
kimseunggyu <dev.seunggyu@gmail.com>
2019-02-28 10:00:10 +0900

    clean score code

--- score/IRC3/IRC3.py
+++ score/IRC3/IRC3.py
@@ -1,10 +1,3 @@
-"""
-Programmer    : 김승규, 정해민 - pair programming
-description   : IRC3 - NFT IMPLEMENTATION
-Update Date   : 2019.02.27
-Update        : FINISH
-"""
-
 from iconservice import *
 
 TAG = 'IRC3'
@@ -20,19 +13,18 @@ class IRC3(IconScoreBase):
 
     def __init__(self, db: IconScoreDatabase) -> None:
         super().__init__(db)
-        # 모든 토큰
+        # number of token
         self._total_token = VarDB("TOTALTOKEN", db, value_type=int)
-        # 토큰[토큰id] 로 토큰 소유자 계정 받음
-        self._token_owner = DictDB("TOKEN_OWNER", db, value_type=Address)
-        # approve address를 받음
-        self._approve_address = DictDB("APPROVED_ADDRESS", db, value_type=Address)
-        ## token 저장하는 DB - 이거는 에러 처리를 위해 생성! 과연 이게 필요한가?
+        # token info
         self._token = DictDB("TOKEN", db, value_type=str)
+        # owner of token
+        self._token_owner = DictDB("TOKEN_OWNER", db, value_type=Address)
+        # approve address
+        self._approve_address = DictDB("APPROVED_ADDRESS", db, value_type=Address)
 
     def on_install(self) -> None:
         super().on_install()
         self._total_token.set(0)
-        # self._total_token.set(3)
 
     def on_update(self) -> None:
         super().on_update()
@@ -48,6 +40,10 @@ class IRC3(IconScoreBase):
 
     @external(readonly=True)
     def balanceOf(self, _owner: Address) -> int:
+        # 예외 처리 - NFTs assigned to the zero address are considered invalid
+        if _owner == Address.from_string("cx"+str(0)*40) or _owner == Address.from_string("hx"+str(0)*40):
+            raise IconScoreException("NFTs assigned to the zero address are considered invalid")
+
         count = 0
 
         totalCount = self._total_token.get()
@@ -59,55 +55,57 @@ class IRC3(IconScoreBase):
 
     @external(readonly=True)
     def ownerOf(self, _tokenId: int) -> Address:
+        # 예외처리 - 토큰의 유효성 검증
+        if self.getTotalToken() <= _tokenId:
+            raise IconScoreException("_tokenId is not a valid NFT")
+
         return self._token_owner[_tokenId]
 
     @external
     def getApproved(self, _tokenId: int) -> Address:
+        # 예외처리 -token이 유효하지 않으면
+        if self.getTotalToken() <= _tokenId:
+            raise IconScoreException("_tokenId is not a valid NFT")
+
+        # 예외처리 - If there is none, returns the zero address
+        if not self._approve_address[_tokenId]:
+            return 0
+
         return self._approve_address[_tokenId]
 
     @external
     def approve(self, _to: Address, _tokenId: int):
-        # my address -> _to address -> tokenid
-        # approve_address = _to
-
-        # 호출한 사람이 토큰을 가지고 잇는지 확인
+        # 예외처리 - 호출한 사람이 토큰을 가지고 있는지 확인
         if self.msg.sender != self._token_owner[_tokenId]:
-            # 에러 처리
-            raise IconScoreException("approve : 너 이거 못보내(너 이 토큰 없잖아!~~) ")
+            raise IconScoreException("Throws unless self.msg.sender is the current NFT owner.")
 
-        # 내가 나한테 approve를 못함
+        # 예외처리 - 내가 나한테 approve를 못함
         if self.msg.sender == _to:
-            raise IconScoreException("approve : 너가 너한테 이거 못보내지롱~~ (너 이 토큰 없잖아!~~) ")
-            # pass
+            raise IconScoreException("I can't make an approve of myself.")
 
-        # _to 이미 토큰을 가지고 있는지 확인
+        # 예외처리 - _to 이미 토큰을 가지고 있는지 확인
         if _to == self._token_owner[_tokenId] :
-            raise IconScoreException("approve : to가 이미 소유 중 (니꺼잖아!! 자신의 토큰을 자신에게 approve할 필요 없습니다.")
-             # 에러 처리
-        Logger.warning(f"self._approve_address[_tokenId]: {self._approve_address[_tokenId]}")
-        # token의 소유자를 approve 실행
+            raise IconScoreException("You don't have to approve your token to yourself.")
+
         self._approve_address[_tokenId] = _to
-        Logger.warning(f"self._approve_address[_tokenId]: {self._approve_address[_tokenId]}")
 
     @external
     def transfer(self, _to: Address, _tokenId: int):
-        # 에러처리 1 - Throws unless self.msg.sender is the current owner.
+        # 예외처리 1 - 호출한 사람이 토큰을 가지고 있는지 확인
         if self.msg.sender != self._token_owner[_tokenId]:
-            raise IconScoreException("transfer: self.msg.sender 소유권 없음")
+            raise IconScoreException("Throws unless self.msg.sender is the current owner.")
 
-        ##### Throws if _to is the zero address !!!!!!! 추가 필요, 질문하기
-        # 에러처리 2 - Throws if _tokenId is not a valid NFT
-        if _to == "cx"+str(0)*40 or _to == "hx"+str(0)*40:
-            raise IconScoreException("transfer: ", "_to는 ZERO ADDRESS 가 될 수 없습니다.")
+        # 예외처리 2 - Throws if _tokenId is not a valid NFT
+        if _to == Address.from_string("cx"+str(0)*40) or _to == Address.from_string("hx"+str(0)*40):
+            raise IconScoreException("Throws if _to is the zero address.")
 
-        # 에러처리 3 - Throws if _tokenId is not a valid NFT
-        total_token = self._total_token.get()  # 2
-        if _tokenId > total_token:
-            raise IconScoreException("transfer: ", "올바른 토큰이 아닙니다.")
+        # 예외처리 3 - Throws if _tokenId is not a valid NFT
+        if self.getTotalToken() <= _tokenId:
+            raise IconScoreException("Throws if _tokenId is not a valid NFT.")
 
-        # 에러처리 4 - approve 확인
+        # 예외처리 4 - approve 확인
         if self._approve_address[_tokenId] != _to:
-            raise IconScoreException("transfer: approve의 계정이 아닙니다.")
+            raise IconScoreException("This is not an approve account.")
 
         # 실제적인 transfer 실행
         self._token_owner[_tokenId] = _to
@@ -116,33 +114,23 @@ class IRC3(IconScoreBase):
 
     @external
     def transferFrom(self, _from: Address, _to: Address, _tokenId: int):
+
         ## 오류 1 - Throws unless self.msg.sender is the current owner or the approved address for the NFT
-        # -> self.msg.sender가 소유자거나 approve를 받은 계정이면 안됨
-
-
-        Logger.warning(f"self.msg.sender: {self.msg.sender}", TAG)
-        Logger.warning(f"self._token_owner[_tokenId]: {self._token_owner[_tokenId]}",TAG)
-        Logger.warning(f"_from: {_from}", TAG)
-        # Logger.warning(f"self.getApproved[_tokenId]: {self.getApproved[_tokenId]}")
-
         # if self.msg.sender == self._token_owner[_tokenId] or self.msg.sender == self.getApproved[_tokenId]:
         if self.msg.sender == self._token_owner[_tokenId] or self.msg.sender != self._approve_address[_tokenId]:
-            raise IconScoreException("transferFrom: ", "self.msg.sender가 소유자거나 approve를 받은 계정이면 안됩니다.")
+            raise IconScoreException("Throws unless self.msg.sender is the current owner or the approved address for the NFT")
 
-        Logger.warning(f"_from: {type(_from)}", TAG)
-        Logger.warning(f"self._token_owner[_tokenId]: {type(self._token_owner[_tokenId])}", TAG)
         ## 오류 2 -  Throws if _from is not the current owner.
         if _from != self._token_owner[_tokenId]:
-            raise IconScoreException("transferFrom: _from 이 토큰의 소유자가 아닙니다.")
+            raise IconScoreException("Throws if _from is not the current owner.")
 
         ## 오류 3 - Throws if _to is the zero address.
-        if _to == "cx"+str(0)*40 or _to == "hx"+str(0)*40:
-            raise IconScoreException("transferFrom: ", "_to는 ZERO ADDRESS 가 될 수 없습니다.")
+        if _to == Address.from_string("cx"+str(0)*40) or _to == Address.from_string("hx"+str(0)*40):
+            raise IconScoreException("Throws if _to is the zero address.")
 
-        ## 오류 4 -  Throws if _tokenId is not a valid NFT.
-        total_token = self._total_token.get()  # 2
-        if _tokenId > total_token:
-            raise IconScoreException("transferFrom: ", "올바른 토큰이 아닙니다.")
+        # 예외처리 4 - Throws if _tokenId is not a valid NFT
+        if self.getTotalToken() <= _tokenId:
+            raise IconScoreException("Throws if _tokenId is not a valid NFT.")
 
         # 실제적인 트랜스퍼
         self._token_owner[_tokenId] = _to
@@ -164,10 +152,7 @@ class IRC3(IconScoreBase):
 
     @external
     def setApproveAddress(self, _to:Address, _tokenId:int):
-        Logger.warning(f"1 self._approve_address[_tokenId]: {self._approve_address[_tokenId]}",TAG)
         self._approve_address[_tokenId] = _to
-        Logger.warning(f"2 self._approve_address[_tokenId]: {self._approve_address[_tokenId]}", TAG)
-
 
     @external
     def getTotalToken(self):
